9801191ddffcb4f9b6f211a4f913bc7da60a9da7
Савелий Романов <romanovsavelij@MacBook-Air-Savelij.local>
2019-12-06 16:43:30 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Heaps.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Heaps.dir/BinomialHeap.cpp.o"
+  "CMakeFiles/Heaps.dir/main.cpp.o"
+  "Heaps"
+  "Heaps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Heaps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
